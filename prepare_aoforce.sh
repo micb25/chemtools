a10f5587c4e665bce864da0d98b749e618324daa
Michael Böhme <boehme.mic@gmail.com>
2019-01-08 12:56:31 +0100

    fixed typo

--- prepare_aoforce.sh
+++ prepare_aoforce.sh
@@ -21,7 +21,7 @@ irreps=(a au ag b e)
 # for higher symmetries
 #irreps=(a au ag a1 a2 b1 b2 e eg eu t1 t2 t1g t2g t1u t2u h a1g a2g a1u a2u b1g b2g b1u b2u)
 
-tmpfiles=(ddens dH)
+tmpfiles=(ddens dh)
 symfiles=(vfile_ wfile_ rhs_ cphf_ g_sxi_ sxi_)
 
 for file in ${!symfiles[@]}
